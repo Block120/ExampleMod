c81d4f533b538dc6207d682dd6d4e682d554f7b5
Block120 <165931702+Block120@users.noreply.github.com>
2024-11-24 11:49:36 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,12 +4,12 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
     public const string Version = "1.0.0";
-    public const string Name = "ExampleMod";
+    public const string Name = "ExampleMod(Adds an example tower and example bloon)";
 
-    public const string Description = "Adds a new tower with 21 upgrade and a paragon.";
+    public const string Description = "Adds a new tower with 21 upgrade and a paragon and a new bloon called the lead moab which is a moab with the lead property that appears on round 50.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Block120";
+    public const string RepoName = "ExampleMod";
 
     public const string Dependencies = "doombubbles/paths-plus-plus";
 }
