14e34b198a1c77c7c996f2d4ada9a86b8ea37c99
Block120 <165931702+Block120@users.noreply.github.com>
2024-11-24 11:31:58 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+namespace ExampleMod;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "44.0";
+    public const string Version = "1.0.0";
+    public const string Name = "ExampleMod";
+
+    public const string Description = "Adds a new tower with 21 upgrade and a paragon.";
+
+    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+
+    public const string Dependencies = "doombubbles/paths-plus-plus";
+}
