41792b46cce147d2796a6822137c04813ceaf51c
Block120 <165931702+Block120@users.noreply.github.com>
2024-11-24 17:11:43 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,7 +9,7 @@ public static class ModHelperData
     public const string Description = "Adds a new tower with 21 upgrade and a paragon and a new bloon called the lead moab which is a moab with the lead property that appears on round 50.";
 
     public const string RepoOwner = "Block120";
-    public const string RepoName = "ExampleMod";
+    public const string RepoName = "ExampleModBTD6";
 
     public const string Dependencies = "doombubbles/paths-plus-plus";
 }
