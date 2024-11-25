3d41d2f0e0c59bdfec067d3ec9a6f36b5419ff86
Block120 <165931702+Block120@users.noreply.github.com>
2024-11-25 07:11:33 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
     public const string Version = "1.0.0";
-    public const string Name = "ExampleMod(Adds an example tower and example bloon)";
+    public const string Name = "ExampleMod";
 
     public const string Description = "Adds a new tower with 21 upgrade and a paragon and a new bloon called the lead moab which is a moab with the lead property that appears on round 50.";
 
